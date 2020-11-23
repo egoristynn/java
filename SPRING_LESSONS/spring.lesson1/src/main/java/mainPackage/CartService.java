d515eba2082f9ecd8072f89c275bfe3893b9f78d
Egor <EgoricVetkin@mail.ru>
2020-11-23 12:41:37 +0300

    Выполнил задание к 1 уроку по Spring

---
+++ SPRING_LESSONS/spring.lesson1/src/main/java/mainPackage/CartService.java
@@ -0,0 +1,35 @@
+package mainPackage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Scope("prototype")
+public class CartService {
+
+    ProductRepository productRepository;
+    List<Product> itemList = new ArrayList<>();
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getProductList (){
+        return productRepository.findAll();
+    }
+
+    public List<Product> addItem(Product product){
+        itemList.add(product);
+        return itemList;
+    }
+
+    public List<Product> deleteItem(Product product){
+        itemList.remove(product);
+        return itemList;
+    }
+}
